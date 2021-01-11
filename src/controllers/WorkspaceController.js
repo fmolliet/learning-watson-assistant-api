8b9618545a538b606ee3df4d4edc38fd6dbfa7b6
fmolliet <fmolliet@hotmail.com>
2021-01-11 18:17:54 -0300

    Corrigido tratamento de exceção e exibição de erros

--- src/controllers/WorkspaceController.js
+++ src/controllers/WorkspaceController.js
@@ -7,18 +7,16 @@ export default {
             const result = await WorkspaceServices.indexWorkspaces()
             return res.status(200).json(result.result)
         }catch (err){
-            return res.status(400).json(err)
+            return res.status(400).json({Error: err.message})
         }
     },
     
     async delete(req, res){
         try{
-            if ( !req.body.workspaceId ) throw new Error("Ausencia de parâmetros")
             const result = await WorkspaceServices.deleteWorkspace(req.body)
             return res.status(200).json(result.result)
         }catch (err){
-            console.error(err)
-            return res.status(400).json({"Error":err})
+            return res.status(400).json({Error: err.message})
         }
     }
 }
